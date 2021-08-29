6b65ee19ec4128eb8fd54c0ae7730a7ec3b39b64
Tiago Rodrigues <isatiago098@gmail.com>
2021-08-29 03:08:55 -0300

    resoluçao exercicio musica1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace musica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor=ConsoleColor.Green;
+            Console.WriteLine("Atirei o pau no Gato");
+            Console.WriteLine();
+           Console.ForegroundColor=ConsoleColor.Blue;
+        Console.WriteLine("Atirei o pau no Gato");
+            Console.WriteLine("Atirei o pau no Gato");
+            Console.WriteLine("Atirei o pau no Gato");
+            Console.ForegroundColor=ConsoleColor.Green;
+            Console.WriteLine("Atirei o pau no Gato");
+            Console.WriteLine("Atirei o pau no Gato");
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine("Atirei o pau no Gato");
+            Console.ForegroundColor=ConsoleColor.White;
+            Console.WriteLine("Atirei o pau no Gato");
+
+}
+    }
+}
